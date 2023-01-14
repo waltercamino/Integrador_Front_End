94b355765438ad11a22a61f422af43283a8130e2
Walter <106196739+waltercamino@users.noreply.github.com>
2023-01-14 20:02:29 -0300

    agregado de eventos

---
+++ JavaScr/funciones.js
@@ -0,0 +1,28 @@
+
+document.getElementById("ocultar_experiencia").addEventListener("click", function (){
+    document.getElementById("experiencia").style.display= "none"
+})
+
+
+document.getElementById("mostrar_experiencia").addEventListener("click", function (){
+    document.getElementById("experiencia").style.display= "block"
+})
+
+
+document.getElementById("ocultar_formacion").addEventListener("click", function (){
+    document.getElementById("formacion").style.display= "none"
+})
+
+
+document.getElementById("mostrar_adicional").addEventListener("click", function (){
+    document.getElementById("formacion").style.display= "block"
+})
+
+document.getElementById("ocultar_adicional").addEventListener("click", function (){
+    document.getElementById("adicional").style.display= "none"
+})
+
+document.getElementById("mostrar_adicional").addEventListener("click", function (){
+    document.getElementById("adicional").style.display= "block"
+})
+
